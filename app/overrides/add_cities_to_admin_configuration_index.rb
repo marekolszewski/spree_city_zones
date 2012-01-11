202d9bb8cd2582b148e31c499400a96769701d65
M. Scott Ford <scott@corgibytes.com>
2012-01-11 10:38:54 -0500

    Fixes typo in virtual_path.

--- app/overrides/add_cities_to_admin_configuration_index.rb
+++ app/overrides/add_cities_to_admin_configuration_index.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
   :name => "add_cities_to_admin_configuration_index",
-  :virtual_path => "admin/configuration/index",
+  :virtual_path => "admin/configurations/index",
   :insert_bottom => "[data-hook='admin_configurations_menu']",
   :partial => "shared/cities_admin_configurations_menu")
