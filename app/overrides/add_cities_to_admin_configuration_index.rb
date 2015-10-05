5a9835a7e4d3a13ae8173ba5c68b2d217cb94e6d
Geisimar Rech <falecom@rupix.com.br>
2015-10-05 14:04:26 -0400

    fix item menu

--- app/overrides/add_cities_to_admin_configuration_index.rb
+++ app/overrides/add_cities_to_admin_configuration_index.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(
-  :name => "add_cities_to_admin_configuration_index",
-  :virtual_path => "spree/admin/configurations/index",
-  :insert_bottom => "[data-hook='admin_configurations_menu']",
-  :partial => "spree/shared/cities_admin_configurations_menu")
+  Deface::Override.new(
+    :name => "add_cities_to_admin_configuration_menu",
+    :virtual_path => "spree/admin/shared/_configuration_menu",
+    :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+    :text =>
+     "<%= configurations_sidebar_menu_item Spree.t(:cities), spree.admin_country_state_cities_path(Spree::Config[:default_country_id],Spree::State.find(:first, :conditions => {:country_id => Spree::Config[:default_country_id], :abbr => 'CA'})) %>"
+  )
